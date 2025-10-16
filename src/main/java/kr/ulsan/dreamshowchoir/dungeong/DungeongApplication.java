43675576ffe4eef2ce3a8ece286c74c752d17e34
덩엉이 <dud7757@naver.com>
2025-10-16 12:54:13 +0900

    create

---
+++ src/main/java/kr/ulsan/dreamshowchoir/dungeong/DungeongApplication.java
@@ -0,0 +1,13 @@
+package kr.ulsan.dreamshowchoir.dungeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeongApplication.class, args);
+	}
+
+}
