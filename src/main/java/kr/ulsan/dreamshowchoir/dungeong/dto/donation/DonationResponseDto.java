51061f867a0e311d30927645a5077000cc39d2bc
덩엉이 <dud7757@naver.com>
2025-12-08 21:45:48 +0900

    후원자 정보 추가

--- src/main/java/kr/ulsan/dreamshowchoir/dungeong/dto/donation/DonationResponseDto.java
+++ src/main/java/kr/ulsan/dreamshowchoir/dungeong/dto/donation/DonationResponseDto.java
@@ -16,6 +16,9 @@ public class DonationResponseDto {
     private final DonationType type;
     private final DonationStatus status;
     private final LocalDateTime createdAt;
+    private final String donorName;
+    private final String donorEmail;
+    private final String donorPhone;
 
     /**
      * Entity를 DTO로 변환하는 생성자
@@ -27,5 +30,8 @@ public class DonationResponseDto {
         this.type = donation.getType();
         this.status = donation.getStatus();
         this.createdAt = donation.getCreatedAt();
+        this.donorName = donation.getUser().getName();
+        this.donorEmail = donation.getUser().getEmail();
+        this.donorPhone = donation.getUser().getPhoneNumber();
     }
 }
--- src/main/java/kr/ulsan/dreamshowchoir/dungeong/dto/donation/DonorResponseDto.java
+++ src/main/java/kr/ulsan/dreamshowchoir/dungeong/dto/donation/DonorResponseDto.java
@@ -1,6 +1,7 @@
 package kr.ulsan.dreamshowchoir.dungeong.dto.donation;
 
 import kr.ulsan.dreamshowchoir.dungeong.domain.donation.Donation;
+import kr.ulsan.dreamshowchoir.dungeong.domain.donation.DonationType;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -10,11 +11,13 @@ public class DonorResponseDto {
     private final String donorName; // 후원자 이름
     private final Long amount;      // 후원 금액
     private final LocalDateTime date; // 후원 날짜
+    private final DonationType type;    // 후원 종류
 
     public DonorResponseDto(Donation donation) {
         // Donation 엔티티에 별도 donorName 필드가 있다면 그걸 쓰고, 없으면 User의 이름을 씀
         this.donorName = donation.getUser().getName();
         this.amount = donation.getAmount();
         this.date = donation.getCreatedAt();
+        this.type = donation.getType();
     }
 }
